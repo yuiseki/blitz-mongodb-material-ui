dd597d18443096a7ada5f12d08ee6e72322de0f1
yuiseki <yuiseki@gmail.com>
2021-08-07 15:34:36 +0900

    LoginFormをMaterial-UIにする

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -1,8 +1,7 @@
+import React, { useCallback, useState } from "react"
+import { Button, List, ListItem, TextField } from "@material-ui/core"
 import { AuthenticationError, Link, useMutation, Routes } from "blitz"
-import { LabeledTextField } from "app/core/components/LabeledTextField"
-import { Form, FORM_ERROR } from "app/core/components/Form"
 import login from "app/auth/mutations/login"
-import { Login } from "app/auth/validations"
 
 type LoginFormProps = {
   onSuccess?: () => void
@@ -10,44 +9,78 @@ type LoginFormProps = {
 
 export const LoginForm = (props: LoginFormProps) => {
   const [loginMutation] = useMutation(login)
+  const [error, setError] = useState<null | AuthenticationError>(null)
+
+  const onSubmit = useCallback(
+    async (e) => {
+      e.preventDefault()
+      const values = {
+        email: e.target.email.value,
+        password: e.target.password.value,
+      }
+      try {
+        await loginMutation(values)
+      } catch (e) {
+        if (e instanceof AuthenticationError) {
+          setError(e)
+        }
+      }
+    },
+    [loginMutation]
+  )
 
   return (
-    <div>
-      <h1>Login</h1>
-
-      <Form
-        submitText="Login"
-        schema={Login}
-        initialValues={{ email: "", password: "" }}
-        onSubmit={async (values) => {
-          try {
-            await loginMutation(values)
-            props.onSuccess?.()
-          } catch (error) {
-            if (error instanceof AuthenticationError) {
-              return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
-            } else {
-              return {
-                [FORM_ERROR]:
-                  "Sorry, we had an unexpected error. Please try again. - " + error.toString(),
-              }
-            }
-          }
-        }}
-      >
-        <LabeledTextField name="email" label="Email" placeholder="Email" />
-        <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
-        <div>
-          <Link href={Routes.ForgotPasswordPage()}>
-            <a>Forgot your password?</a>
-          </Link>
-        </div>
-      </Form>
-
-      <div style={{ marginTop: "1rem" }}>
-        Or <Link href={Routes.SignupPage()}>Sign Up</Link>
-      </div>
-    </div>
+    <form onSubmit={onSubmit}>
+      <List>
+        <ListItem>
+          <h2>ログイン</h2>
+        </ListItem>
+        <ListItem>
+          <TextField
+            type="email"
+            id="email"
+            name="email"
+            required
+            label="メールアドレス"
+            variant="outlined"
+            placeholder="example@example.com"
+          />
+        </ListItem>
+        <ListItem>
+          <TextField
+            type="password"
+            id="password"
+            name="password"
+            required
+            label="パスワード"
+            variant="outlined"
+            placeholder="********"
+          />
+        </ListItem>
+        {error && (
+          <ListItem role="alert" style={{ color: "red" }}>
+            メールアドレスまたはパスワードが間違っています
+          </ListItem>
+        )}
+        <ListItem>
+          <Button type="submit" variant="contained" color="primary">
+            ログイン
+          </Button>
+        </ListItem>
+        <ListItem>
+          <span style={{ marginTop: "2rem" }}>
+            <Link href={Routes.ForgotPasswordPage()}>
+              <a>パスワードを忘れた？</a>
+            </Link>
+          </span>
+        </ListItem>
+        <ListItem>
+          <span style={{ marginTop: "0.5rem" }}>
+            または <Link href={Routes.SignupPage()}>ユーザー登録</Link>
+          </span>
+        </ListItem>
+      </List>
+    </form>
   )
 }
 
