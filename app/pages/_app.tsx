4016e7b4e3913a9555a76e77600a9ea23973f163
yuiseki <yuiseki@gmail.com>
2021-08-07 12:13:44 +0900

    use Material-UI

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -9,16 +9,30 @@ import {
 } from "blitz"
 import LoginForm from "app/auth/components/LoginForm"
 
+import CssBaseline from "@material-ui/core/CssBaseline"
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import React from "react"
+
+//You can customize this as you want and even move it out to a separate file
+const theme = createMuiTheme({
+  palette: {
+    type: "light",
+  },
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
-    <ErrorBoundary
-      FallbackComponent={RootErrorFallback}
-      onReset={useQueryErrorResetBoundary().reset}
-    >
-      {getLayout(<Component {...pageProps} />)}
-    </ErrorBoundary>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ErrorBoundary
+        FallbackComponent={RootErrorFallback}
+        onReset={useQueryErrorResetBoundary().reset}
+      >
+        {getLayout(<Component {...pageProps} />)}
+      </ErrorBoundary>
+    </ThemeProvider>
   )
 }
 
